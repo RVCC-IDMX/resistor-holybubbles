d6602305ffc13fe809344808b0fa18ace7c0fadb
holybubbles <kennycastro007@gmail.com>
2023-04-17 14:15:29 -0400

    completed resistor calculator

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 import { getResistorOhms } from './resistor.js';
+
+let resistorState = {
+    color1: 'black',
+    color2: 'black',
+    multiplier: 'black',
+    tolerance: 'brown'
+};
+
+const band1 = document.querySelector('#band1');
+const band2 = document.querySelector('#band2');
+const bandMultiplier = document.querySelector('#band-multiplier');
+const bandTolerance = document.querySelector('#band-tolerance');
+const updateBtn = document.querySelector("#update-btn");
+
+// updateBtn.addEventListener("click", updateResistor);
+
+band1.addEventListener("change", updateResistor);
+band2.addEventListener("change", updateResistor);
+bandMultiplier.addEventListener("change", updateResistor);
+bandTolerance.addEventListener("change", updateResistor);
+
+
+function updateResistor() {
+    const color1 = band1.value;
+    const color2 = band2.value;
+    const colorMultiplier = bandMultiplier.value;
+    const colorTolerance = bandTolerance.value;
+
+    
+    const resistorBand1 = document.querySelector('#b0');
+    const resistorBand2 = document.querySelector('#b1');
+    const resistorBand3 = document.querySelector('#b2');
+    const resistorBand4 = document.querySelector('#b3');
+
+    resistorBand1.classList.remove(resistorState.color1);
+    resistorBand2.classList.remove(resistorState.color2);
+    resistorBand3.classList.remove(resistorState.multiplier);
+    resistorBand4.classList.remove(resistorState.tolerance);
+
+    resistorBand1.classList.add(color1);
+    resistorBand2.classList.add(color2);
+    resistorBand3.classList.add(colorMultiplier);
+    resistorBand4.classList.add(colorTolerance);
+
+    resistorState = {
+        color1,
+        color2,
+        multiplier: colorMultiplier,
+        tolerance: colorTolerance
+    }
+    document.querySelector('.answer').innerText = getResistorOhms(resistorState); 
+}
+
+updateResistor();
--- resistor.js
+++ resistor.js
@@ -9,6 +9,131 @@
    http://bit.ly/2NjS274
 */
 
-const getResistorOhms = (bands) => {};
+const colorCodes = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+const multiplierCodes = {
+  black: 1,
+  brown: 10,
+  red: 100,
+  orange: 1000,
+  yellow: 10000,
+  green: 100000,
+  blue: 1000000,
+  violet: 10000000,
+  grey: 100000000,
+  white: 1000000000,
+  gold: 0.1,
+  silver: 0.01,
+};
+
+const toleranceCodes = {
+  brown: '±1%',
+  red: '±2%',
+  green: '±0.5%',
+  blue: '±0.25%',
+  violet: '±0.1%',
+  grey: '±0.05%',
+  gold: '±5%',
+  silver: '±10%',
+};
+
+function getColorValue(color) {
+  const colorCodes = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+  return colorCodes[color];
+}
+
+function getMultiplierValue(color) {
+  const multiplierCodes = {
+    black: 1,
+    brown: 10,
+    red: 100,
+    orange: 1000,
+    yellow: 10000,
+    green: 100000,
+    blue: 1000000,
+    violet: 10000000,
+    grey: 100000000,
+    white: 1000000000,
+    gold: 0.1,
+    silver: 0.01,
+  };
+  return multiplierCodes[color];
+}
+
+function getThreeBandValue(bands) {
+  // write your code here & return value
+  const temp = (getColorValue(bands.color1) * 10 + getColorValue(bands.color2))
+    * getMultiplierValue(bands.multiplier);
+
+  if (bands.multiplier === 'gold') {
+    temp.toFixed(1);
+  } else if (bands.multiplier === 'silver') {
+    temp.toFixed(2);
+  }
+  return temp;
+}
+
+function formatNumber(val) {
+  // from stackoverflow: https://stackoverflow.com/a/32638472
+  const abbreviateNumber = (num, fixed) => {
+    if (num === null) { return null; } // terminate early
+    if (num === 0) { return '0'; } // terminate early
+    const f = (!fixed || fixed < 0) ? 0 : fixed; // number of decimal places to show
+    const b = (num).toPrecision(2).split('e'); // get power
+    const k = b.length === 1 ? 0
+      : Math.floor(Math.min(b[1].slice(1), 14) / 3); // floor at decimals, ceiling at trillions
+    const c = k < 1 ? num.toFixed(0 + f)
+      : (num / 10 ** (k * 3)).toFixed(1 + f); // divide by power
+    const d = c < 0 ? c : Math.abs(c); // enforce -0 is 0
+    const e = d + ['', 'k', 'M', 'G', 'T'][k]; // append power
+    return e;
+  };
+
+  return abbreviateNumber(val);
+}
+
+function getTolerance(color) {
+  // write your code here & return value
+  const toleranceCodes = {
+    brown: '±1%',
+    red: '±2%',
+    green: '±0.5%',
+    blue: '±0.25%',
+    violet: '±0.1%',
+    grey: '±0.05%',
+    gold: '±5%',
+    silver: '±10%',
+  };
+
+  return toleranceCodes[color];
+}
+
+
+const getResistorOhms = (bands) => { 
+  // write your code here & return value
+  return `Resistor value: ${formatNumber(getThreeBandValue(bands))} Ohms ${getTolerance(bands.tolerance)}`;
+};
 
 export { getResistorOhms };
